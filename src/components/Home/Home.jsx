7935a90ae3e72574f3407366adda91fdacf3346a
MuhammadRizo <odilovadiyora52@gmail.com>
2023-07-08 23:51:04 +0500

    Hi, this's working 🥳 yeah

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+import logo from '../assets/Logo.svg'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <header>
+            <img src={logo} alt="logo" />
+            <h1>ggg</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Home
